e0f4e5e1a5b401fe7194ffd2c914ff9e5a77d797
Alex Withington-Smith <alex@awithsmith.com>
2021-03-25 18:37:31 +0000

    node server and angular now talk to each other

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import cors from 'cors';
+
 const port = 4201;
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/api/test', (req, res) => {
   res.send({ data: 'Hello world!' });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'five-stack-me';
+export class AppComponent implements OnInit {
+  message: String = '';
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http
+      .get('http://localhost:4201/api/test')
+      .subscribe((response: any) => {
+        this.message = response.data;
+      });
+  }
 }
